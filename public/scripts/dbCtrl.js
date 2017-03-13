066ea0c5f3382472192943f03c66feb1917dfaa0
theadammason <adam.mason86@gmail.com>
2017-03-13 13:13:37 -0400

    merge

--- public/scripts/dbCtrl.js
+++ public/scripts/dbCtrl.js
@@ -2,7 +2,7 @@ var app = angular.module('myMod');
 
 app.controller('dbCtrl', function($scope, myFactory){
   myFactory.getStudent().then(function(){
-    $scope.student = myFactory.update();
-    console.log($scope.student);
+    $scope.students = myFactory.update();
+    console.log($scope.students);
   });
 });
