518e62000f0083cf1e13a9a5e654754cede29978
Christina L <clee04@baker.edu>
2017-03-14 15:51:17 -0400

    idk

--- public/scripts/dbCtrl.js
+++ public/scripts/dbCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('myMod');
 
-app.controller('dbCtrl', function($scope, myFactory){
+app.controller('dbCtrl', function($scope, myFactory, readingFactory){
   myFactory.getStudent().then(function(){
     $scope.students = myFactory.update();
     console.log($scope.students);
