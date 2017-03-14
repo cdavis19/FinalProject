63cb0bf9711fd1b19674c25eb03b9adb5f745e77
theadammason <adam.mason86@gmail.com>
2017-03-14 14:18:37 -0400

    fixed search with hosted db

--- public/scripts/dbCtrl.js
+++ public/scripts/dbCtrl.js
@@ -3,12 +3,12 @@ var app = angular.module('myMod');
 app.controller('dbCtrl', function($scope, $animate, myFactory){
   myFactory.getStudent().then(function(){
     $scope.students = myFactory.update();
-    // console.log($scope.students);
+    console.log("$scope.students="+ $scope.students);
   });
 
   $scope.student = {};
   $scope.getUser = function(){
-    // console.log($scope.student.name);
+    console.log($scope.student);
     var enteredName = $scope.students.map(function(x){
       return x.name;
     }).indexOf($scope.student.name);
