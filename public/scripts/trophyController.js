6993fcb312ae1915b4a896b73ae319afdfa928f8
RedEis91 <imitationkrabs91@gmail.com>
2017-03-15 14:53:25 -0400

    trophy room is poppin

--- public/scripts/trophyController.js
+++ public/scripts/trophyController.js
@@ -5,9 +5,33 @@ app.controller('trophyController', function($scope, studentFactory){
     console.log($scope.individual);
 
    if ($scope.individual.booksread >= 100){
+     $('#hundredthBookTrophy').removeClass('locked');
+     console.log($scope.individual.booksread);
+       
+   }else{
+     }
+    
+    if ($scope.individual.booksread >= 10){
+     $('#tenthBookTrophy').removeClass('locked');
+     console.log($scope.individual.booksread);
+     
+   }else{
+     }
+    
+    if ($scope.individual.booksread >= 5){
+     $('#fifthBookTrophy').removeClass('locked');
+     console.log($scope.individual.booksread);
+    
+   }else{
+     }
+    
+   if ($scope.individual.booksread >= 1){
      $('#firstBookTrophy').removeClass('locked');
      console.log($scope.individual.booksread);
        console.log('mission success!!');
    }else{
      }
+    
+    
+    
 });
