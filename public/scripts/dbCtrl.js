99c737e12c5275df1b5bb801d57a7b7478cf69f9
RedEis91 <imitationkrabs91@gmail.com>
2017-03-16 11:01:51 -0400

    woo that was so much fun

--- public/scripts/dbCtrl.js
+++ public/scripts/dbCtrl.js
@@ -9,7 +9,7 @@ app.controller('dbCtrl', function($scope, $animate, dbFactory, readingFactory, s
       $scope.students = dbFactory.update();
       console.log("$scope.students=" + $scope.students);
   });
-
+//taking the name as a variable called student in the ng-model and using that name to search our database, returns the entire row based on that
     $scope.student = {};
     $scope.getUser = function() {
         console.log($scope.student);
@@ -19,7 +19,7 @@ app.controller('dbCtrl', function($scope, $animate, dbFactory, readingFactory, s
         $scope.selectedStudent = $scope.students[index];
 
         studentFactory.sendStudent($scope.selectedStudent);
-
+//displays the message shown on login
         if (!$scope.display) {
             $scope.display = !$scope.display;
         } else {
--- public/scripts/trophyController.js
+++ public/scripts/trophyController.js
@@ -4,34 +4,27 @@ app.controller('trophyController', function($scope, studentFactory){
   $scope.individual = studentFactory.returnStudent();
     console.log($scope.individual);
 
-   if ($scope.individual.booksread >= 100){
-     $('#hundredthBookTrophy').removeClass('locked');
-     console.log($scope.individual.booksread);
-       
-   }else{
-     }
     
-    if ($scope.individual.booksread >= 10){
-     $('#tenthBookTrophy').removeClass('locked');
-     console.log($scope.individual.booksread);
-     
-   }else{
-     }
-    
-    if ($scope.individual.booksread >= 5){
-     $('#fifthBookTrophy').removeClass('locked');
-     console.log($scope.individual.booksread);
-    
-   }else{
-     }
+
     
    if ($scope.individual.booksread >= 1){
-     $('#firstBookTrophy').removeClass('locked');
-     console.log($scope.individual.booksread);
-       console.log('mission success!!');
+     $('#firstBookTrophy').removeClass('locked').parent().parent().addClass('flipper');
    }else{
      }
     
+     if ($scope.individual.booksread >= 5){
+     $('#fifthBookTrophy').removeClass('locked').parent().parent().addClass('flipper');
+   }else{
+     }
+    
+      if ($scope.individual.booksread >= 10){
+     $('#tenthBookTrophy').removeClass('locked').parent().parent().addClass('flipper');
+   }else{
+     }
+    
+   if ($scope.individual.booksread >= 100){  $('#hundredthBookTrophy').removeClass('locked').parent().parent().addClass('flipper');
+   }else{
+     }
     
     
 });
