a9ea44ccf9be6d6c97776e935805f3b445d80c7d
RedEis91 <imitationkrabs91@gmail.com>
2017-03-16 16:33:47 -0400

    added notes

--- public/scripts/dbCtrl.js
+++ public/scripts/dbCtrl.js
@@ -1,7 +1,8 @@
+//initialize module
 var app = angular.module('myMod');
 
 
-
+//initializes dbCtrl
 app.controller('dbCtrl', function($scope, $animate, dbFactory, readingFactory, studentFactory, $location) {
 
 
@@ -13,12 +14,15 @@ app.controller('dbCtrl', function($scope, $animate, dbFactory, readingFactory, s
     //taking the name as a variable called student in the ng-model and using that name to search our database, returns the entire row based on that
     $scope.student = {};
     $scope.getUser = function() {
-        console.log($scope.student);
+   //runs .map on $scope.students array looks through it pulls data from it, and creates a new array
         var index = $scope.students.map(function(x) {
+            //array with student names
             return x.studentname;
+            //finds index of each name and initializes selectedstudent with the value of the item in $scope.students with that index
+            //runs map on each item in the array, spits out student name, if student name matches the name typed, it assigns selectedstudent with the value of the item in $scope.students with that index
         }).indexOf($scope.student.studentname);
         $scope.selectedStudent = $scope.students[index];
-
+//uses sendStudent method from studentFactory on selectedStudent object
         studentFactory.sendStudent($scope.selectedStudent);
         //displays the message shown on login
         if (!$scope.loginConfirmMessage) {
@@ -27,6 +31,7 @@ app.controller('dbCtrl', function($scope, $animate, dbFactory, readingFactory, s
 
         };
 
+        //functions to remove login button and register button using ngShow and ngHide
         if (!$scope.loginButton) {
             $scope.loginButton = !$scope.loginButton;
             $scope.registerButton = !$scope.registerButton;
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+//requires express & pg in order to run
 var express = require('express');
 var pg = require('pg');
+//calling express 
 var app = express();
+//stores our password
 var password = require('./password');
+//the login information that is required in order to use database
 var connectionString = 'postgres://uucfqjmvphfcff:' + password + '@ec2-50-17-236-15.compute-1.amazonaws.com:5432/de5hv9qg2ieort?ssl=true';
+//requires bodyParser to run
 var bodyParser = require('body-parser');
+//sets the client using the connection as an argument
 var client = new pg.Client(connectionString);
 
+
+
+//stores information required to determine what database we are pulling from in an object called config
 var config = {
 
     user: 'uucfqjmvphfcff',
@@ -16,23 +25,25 @@ var config = {
     max: 100,
     idleTimeoutMillis: 30000
 };
-
+//pool is where requests to server are held
 var pool = new pg.Pool(config);
-
+//bodyParser sorts or parses through the body of the request
 app.use(bodyParser.json({
     extended: true
 }));
+//tells express to run from the directory called public
 app.use(express.static(__dirname + '/public'));
-
+//this Get function gets the entire table from the database and uses "query" to store the results from the get request in an empty array results
 app.get('/get-student', function(req, res) {
     var results = [];
     pg.connect(connectionString, function(err, client, done) {
-
+//selects everything from the students info table
         var query = client.query('select * from studentsinfo');
-
+//going thru each row of our database and pushes each row to results array
         query.on('row', function(row) {
             results.push(row);
         });
+        //this stops the database from pushing each row to the results array
         query.on('end', function() {
             client.end();
             return res.json(results);
@@ -40,28 +51,31 @@ app.get('/get-student', function(req, res) {
     });
 });
 
+//this function adds new data to our table/database
 app.post('/add-student', function(req, res, next) {
     var results = [];
     var data = {
         studentname: req.body.studentname
     };
-
+//connection is our address to the database, this allows the ability to access the information contained in the database
     pg.connect(connectionString, function(err, client, done) {
+        //this query inserts a new row into the table based on data.studentname from the user input, and automatically sets booksread to zero
         client.query('INSERT INTO studentsinfo(studentname, booksread) values($1, $2)', [data.studentname, 0]);
-
+//this query pulls all of the data from the table
         var query = client.query('SELECT * FROM studentsinfo');
-
+//going thru each row of our database and pushes each row to results array
         query.on('row', function(row) {
             results.push(row);
         });
+        //this stops the database from pushing each row to the results array
         query.on('end', function() {
             client.end();
             return res.json(results);
-            console.log(results);
         });
     });
 });
 
+//this function updates existing data in the table/database i.e. updates number of books
  app.put('/update-books-read/:id', function(req, res, next) {
      var results = [];
      var id = req.params.id;
@@ -69,15 +83,19 @@ app.post('/add-student', function(req, res, next) {
        booksread: req.body.booksread
      };
 
-
+//updates the booksread field in table
      pg.connect(connectionString, function(err, client, done) {
        client.query('UPDATE studentsinfo SET booksread=($1) WHERE id=($2)', [data.booksread, id]);
-     var query = client.query('SELECT * FROM studentsinfo');
+     
+         //this query pulls all of the data from the table
+         var query = client.query('SELECT * FROM studentsinfo');
 
      query.on('row', function(row){
            results.push(row);
 
          });
+         
+          //this stops the database from pushing each row to the results array
          query.on('end', function(){
            client.end();
            return res.json(results);
@@ -87,7 +105,7 @@ app.post('/add-student', function(req, res, next) {
      });
 
 
-
+//this sets our server to localhost:3000
 var server = app.listen(3000, function() {
     var port = server.address().port;
     console.log('PostgreSQL server running at http://localhost:%s', port);
